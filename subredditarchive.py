c1f0b1e5beea37583295bfa72744bfc430381710
peoplma <peoplma@gmail.com>
2015-07-19 02:38:24 -0400

    Update subredditarchive.py

--- subredditarchive.py
+++ subredditarchive.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 import praw
 import random
@@ -5,15 +6,30 @@ import re
 import urllib2
 import sys
 import traceback
+import requests
 from pprint import pprint
+import sys
+print "      __     __ _                            _                        _     _                "
+print "     / /    / /| |                          (_)                      | |   (_)               "
+print "    / / __ / /_| | ___   __ _  ___  ___ ___  _ _ __     __ _ _ __ ___| |__  ___   _____ _ __ "
+print "   / / '__/ / _` |/wow\ / _` |/ _ \/ __/ _ \| | '_ \   / _` | '__/ __| '_ \| \ \ / / _ \ '__|"
+print "  / /| | / / (_| | (_) | (_| |  __/ (_| (_) | | | | | | (_| | | | (__| | | | |\ V /  __/ |  "
+print " /_/ |_|/_/ \__,_|\___/ \__, |\___|\___\___/|_|_| |_|  \__,_|_|  \___|_| |_|_| \_/ \___|_| "
+print "                         __/ |   "
+print "                        |___/   "
+print "Code written by: /u/peoplma and /u/healdb"
+print "Sillines added by: /u/joshtheimpaler"
+print "Wow by: /r/dogecoin"
+#This was annoying during my testing
+#time.sleep(5)
 
 subName = 'dogecoin'
 r = praw.Reddit('searchandarchive')
 a = 'timestamp:1386460800..1386547200'
 b = 'timestamp:'
-c = str(1391212800)
+c = str(1386639720)
 d = '..'
-e = str(1393632000)
+e = str(1437240630)
 r.config.store_json_result = True
 def main():
 	def my_range(start, end, step):
@@ -21,35 +37,27 @@ def main():
 			yield start
 			start += step
 
-	for c in my_range(1391212800, 1393632000, 60):
+	for c in my_range(1386641400, 1437240630, 30):
 		f = str(c)
-		g = str(c+60)
-		print c
-		search_results = r.search(b+f+d+g, subreddit=subName, sort='new', syntax='cloudsearch')
-		if c >= 1393632000:
-			print 'DONE!!!!!!!!'
+		g = str(c+30)
+		search_results = r.search(b+f+d+g, subreddit=subName, syntax='cloudsearch')
+		if c == 1437240630:
+			print 'Welp, all done here! Stopped at timestamp' , c
+			quit()
 		else:
 			for post in search_results:
-				print post
+				print "I found a post! It is called:" , post
+				url= (post.permalink).replace('?ref=search_posts','')
 				#pprint(vars(post))
-				post_json = post.json_dict
-				post_jsonstr = str(post_json)
-				open('rdogecoinarchive.txt', 'ab+')
-				obj = open('rdogecoinarchive.txt', 'ab+')
-				obj.write(post_jsonstr)
+				data= {'user-agent':'archive by /u/healdb'}
+				#manually grabbing this file is much faster than loading the individual json files of every single comment, as this json provides all of it
+				response = requests.get(url+'.json',headers=data)
+				#Create a folder called dogecoinArchive before running the script
+				filename='dogecoinArchive/'+post.name
+				obj=open(filename, 'w')
+				obj.write(response.text)
 				obj.close()
 				#print post_json
-				post.replace_more_comments(limit=10000, threshold=-10000)
-				flat_comments = praw.helpers.flatten_tree(post.comments)
-				for comment in flat_comments:
-					comment_json = comment.json_dict
-					#print comment_json
-					comment_jsonstr = str(comment_json)
-					open('rdogecoinarchive.txt', 'ab+')
-					obj = open('rdogecoinarchive.txt', 'ab+')
-					obj.write(comment_jsonstr)
-					obj.close()
-	
 def secondary():
 	try:
 		while True:
@@ -60,4 +68,3 @@ def secondary():
 		time.sleep(120)
 while True:
 	secondary()
-	
